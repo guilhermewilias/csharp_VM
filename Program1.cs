a8ce4371716c3227c26571bb4e0bc806f0736cfd
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-12-01 13:34:59 -0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,19 @@
+﻿/*
+ Dada uma seqüência de n números (vetor de inteiros), imprimi-la na ordem inversa que foi realizada a leitura
+ */
+
+static void main()
+{
+    int[] numeros = { 1, 2, 3, 4, 5 };
+
+    Array.Reverse(numeros);
+
+    Console.WriteLine("Sequência dos números invertidos : ");
+    foreach (int numero in numeros)
+    {
+           Console.WriteLine(numero + " ");
+    }
+
+    Console.ReadKey();
+}
+main();
